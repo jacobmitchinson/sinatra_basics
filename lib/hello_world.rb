a5f2a793aacb9fa5fd6b06e92dca6b94fcd3c659
Jacob Mitchinson <jmitchinson+github@gmail.com>
2014-12-23 20:18:07 +0000

    basic sinatra app

---
+++ lib/hello_world.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+
+get '/' do 
+  "Hello, world!"
+end
+
+get '/about' do 
+  "I'm Jake. Yes I am"
+end
+
+get '/hello/:name' do
+  "Hello there #{params[:name].capitalize}"
+end
+
+get '/form' do 
+  erb :form
+end
+
+post '/form' do 
+  "You said: #{params[:message]}"
+end
+
+get '/secret' do
+  erb :secret
+end
+
+post '/secret' do 
+  params[:secret].reverse
+end
+
+get '/decrypt/:secret' do 
+  "Here's eyes only: "
+  params[:secret].reverse
+end
+
+not_found do 
+  status 404
+  'not found'
+end
